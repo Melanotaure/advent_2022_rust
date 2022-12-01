6804d943621fa3c95792c8e1daedea0077f0331a
Simon Diradourian <simond@kickmaker.net>
2022-12-01 08:41:00 +0100

    day1 part 2

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -1,22 +1,26 @@
 pub fn run_part(_input: &String, _part: u8) {
-    let mut max: u32 = 0;
+    let mut all_values = vec!();
     let mut current: u32 = 0;
+
     for l in _input.lines() {
         if l.len() != 0 {
             let value: u32 = l.parse().unwrap();
             current = current + value;
-        }
-        if current > max {
-            max = current;
-        }
-        if l.len() == 0 {
+        } else {
+            all_values.push(current);
             current = 0;
         }
     }
-    dbg!(max);
+    all_values.sort_by(|a, b| b.cmp(a));
+
+    if _part == 1 {
+        println!("{}", all_values[0]);
+    } else {
+        println!("{}", &all_values[0..3].iter().sum::<u32>());
+    }
 }
 
 pub fn run(_input: &String) {
     run_part(_input, 1);
-    // run_part(_input, 2);
+    run_part(_input, 2);
 }
