9ddda48a199eb078817c13113a5b7f3c53a3db84
gitsimon <simon.diradourian@e-adsi.com>
2022-12-01 07:27:16 +0100

    day1 part 1

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -1,5 +1,19 @@
 pub fn run_part(_input: &String, _part: u8) {
-    
+    let mut max: u32 = 0;
+    let mut current: u32 = 0;
+    for l in _input.lines() {
+        if l.len() != 0 {
+            let value: u32 = l.parse().unwrap();
+            current = current + value;
+        }
+        if current > max {
+            max = current;
+        }
+        if l.len() == 0 {
+            current = 0;
+        }
+    }
+    dbg!(max);
 }
 
 pub fn run(_input: &String) {
